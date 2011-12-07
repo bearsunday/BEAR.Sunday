6ed60409759fb6fba25c5d2d6d1a081a984b82fb
Akihito Koriyama <akihito.koriyama@gmail.com>
2011-12-07 18:34:57 +0900

    Fix case.

--- src.php
+++ src.php
@@ -6,5 +6,5 @@ require_once __DIR__ . '/vendor/Ray.Aop/src.php';
 require_once __DIR__ . '/vendor/Ray.Di/src.php';
 require_once __DIR__ . '/vendor/BEAR.Resource/src.php';
 require_once __DIR__ . '/vendor/Smarty3/libs/Smarty.class.php';
-require_once __DIR__ . '/vendor/haanga/lib/Haanga.php';
+require_once __DIR__ . '/vendor/Haanga/lib/Haanga.php';
 require_once __DIR__ . '/packages/BEAR.Framework/src.php';

af013f2c28adf7428cc619b0ac98bdba947c3d47
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-02-10 16:47:51 +0900

    Add singleton scope in module.

--- apps/01-demo/Module/AppModule.php
+++ apps/01-demo/Module/AppModule.php
@@ -28,15 +28,15 @@ class AppModule extends AbstractModule
     {
         $this->bind('BEAR\Resource\SchemeCollection')->toProvider('\demoWorld\Module\Provider\SchemeCollectionProvider')->in(Scope::SINGLETON);
         $this->bind()->annotatedWith('GreetingMessage')->toInstance(['en' => 'Hello World', 'ja' => 'Konichiwa Sekai']);
-        $this->bind()->annotatedWith('Twig')->toProvider('\demoWorld\Module\Provider\TwigProvider');
-        $this->bind()->annotatedWith('Smarty')->toProvider('\demoWorld\Module\Provider\SmartyProvider');
+        $this->bind()->annotatedWith('Twig')->toProvider('\demoWorld\Module\Provider\TwigProvider')->in(Scope::SINGLETON);;
+        $this->bind()->annotatedWith('Smarty')->toProvider('\demoWorld\Module\Provider\SmartyProvider')->in(Scope::SINGLETON);;
         $helloDi = include dirname(dirname(__DIR__)) . '/00-helloworld-min/script/di.php';
         $this->bind('Ray\Di\InjectorInterface')->annotatedWith('HelloDi')->toInstance($helloDi);
         $this->bind()->annotatedWith('dsn')->toInstance('/tmp/demo01.sqlite3');
         // PDO
-        $this->bind('Ray\Di\ProviderInterface')->annotatedWith('pdo')->to('\demoWorld\Module\Provider\PdoProvider');
+        $this->bind('Ray\Di\ProviderInterface')->annotatedWith('pdo')->to('\demoWorld\Module\Provider\PdoProvider')->in(Scope::SINGLETON);;
         // Doctrine DBAL
-        $this->bind('Doctrine\DBAL\Connection')->annotatedWith('dbal')->toProvider('\demoWorld\Module\Provider\DbalProvider');
+        $this->bind('Doctrine\DBAL\Connection')->annotatedWith('dbal')->toProvider('\demoWorld\Module\Provider\DbalProvider')->in(Scope::SINGLETON);;
         // Annotation
         $this->bindInterceptor($this->matcher->any(), $this->matcher->annotatedWith('Log'), [new Log]);
 //         $this->bindInterceptor($this->matcher->any(), $this->matcher->annotatedWith('Transactional'), [new Transactional]);
--- packages/BEAR.Framework/src/Module/StandardModule.php
+++ packages/BEAR.Framework/src/Module/StandardModule.php
@@ -12,11 +12,15 @@ class StandardModule extends AbstractModule
 {
     private $injector;
     private $appName;
-    
-    public function __construct(Inject $injector, $appName){
+
+    public function __construct(Inject $injector, $appName)
+    {
+        $this->bindings = new \ArrayObject;
+        $this->pointcuts = new \ArrayObject;
+        $this->container = new \ArrayObject;
         $this->injector = $injector;
+        $this->config = $injector->getContainer()->getForge()->getConfig();
         $this->appName = $appName;
-        parent::__construct();
         $this->configure();
     }
 
@@ -24,7 +28,7 @@ class StandardModule extends AbstractModule
     {
         $this->bind()->annotatedWith('AppName')->toInstance($this->appName);
         $this->bind('Ray\Di\InjectorInterface')->toInstance($this->injector);
-        $this->bind('Ray\Di\ConfigInterface')->toInstance($this->injector->getContainer()->getForge()->getConfig());
+        $this->bind('Ray\Di\ConfigInterface')->toInstance($this->config);
         $this->bind('BEAR\Resource\Resource')->to('BEAR\Resource\Client');
         $this->bind('BEAR\Resource\Invokable')->to('BEAR\Resource\Invoker');
         $this->bind('BEAR\Resource\Linkable')->to('BEAR\Resource\Linker');

eccf5dcf02ce5afde1abea106290b3fafe4f2d36
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-02-21 14:23:08 +0900

    $_ENV['BEAR_OUTPUT_MODE']

--- apps/demoworld/scripts/exception_handler/standard_handler.php
+++ apps/demoworld/scripts/exception_handler/standard_handler.php
@@ -4,13 +4,14 @@
  */
 namespace BEAR\Framework;
 
-use Aura\Autoload\Exception\NotFound;
+    use BEAR\Framework\Exception\ResourceNotFound;
 use BEAR\Resource\Exception\BadRequest;
+use demoworld\Page\Code;
 
 set_exception_handler(function(\Exception $e) {
+    $mode = isset($_ENV['BEAR_OUTPUT_MODE']) ? $_ENV['BEAR_OUTPUT_MODE'] : 'prod';
     try {
-        include_once dirname(dirname(__DIR__)) . '/Page/Code.php';
-        $response = new \demoworld\Page\Code;
+        $response = new Code;
         throw $e;
     } catch (NotFound $e) {
         $response->code = 404;
@@ -24,11 +25,21 @@ set_exception_handler(function(\Exception $e) {
         $response->code = 405;
         $response->body = 'The requested method is not allowed for this URI.';
         goto METHOD_NOT_ALLOWED;
+    } catch (ResourceNotFound $e) {
+        $response->code = 404;
+        $response->headers['X-EXCEPTION'] = get_class($e);
+        $response->headers['X-EXCEPTION-CODE'] = $e->getCode();
+        $response->headers['X-EXCEPTION-MESSAGE'] = $e->getMessage();
+        $response->body = 'The requested URI was not found on this service.';
+        goto NOT_FOUND;
     } catch (\Exception $e) {
         $response->code = 500;
         $response->headers['X-EXCEPTION'] = get_class($e);
         $response->headers['X-EXCEPTION-CODE'] = $e->getCode();
         $response->headers['X-EXCEPTION-MESSAGE'] = $e->getMessage();
+        $response->headers['X-EXCEPTION-TRACE0'] = json_encode($e->getTrace()[0]);
+        $response->headers['X-EXCEPTION-TRACE1'] = json_encode($e->getTrace()[1]);
+        $response->body = '<div class="trace">' . $e->getTraceAsString() . '</div>';
         $response->body = '<div class="trace">' . $e->getTraceAsString() . '</div>';
         goto SERVER_ERROR;
     }
@@ -41,7 +52,7 @@ NOT_FOUND:
 BAD_REQUEST:
 METHOD_NOT_ALLOWED:
 SERVER_ERROR:
-    include dirname(__DIR__) . '/output/prod.output.php';
+    include dirname(__DIR__) . "/output/{$mode}.output.php";
     exit(1);
 }
 );

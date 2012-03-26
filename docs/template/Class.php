0fa05cc810fa2096a5542eb4d9168192fd67bd14
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-03-26 22:11:47 +0900

    Add template.

---
+++ docs/template/Class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  BEAR.Framework
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Framework;
+
+use ArrayObject;
+
+/**
+ * ClassName
+ *
+ * @package BEAR.Framework
+ * @author  Akihito Koriyama <akihito.koriyama@gmail.com>
+ */
+class ClassName
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ docs/template/Trait.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  BEAR.Framework
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Framework;
+
+use ArrayObject;
+
+/**
+ * TraitName
+ *
+ * @package BEAR.Framework
+ * @author  Akihito Koriyama <akihito.koriyama@gmail.com>
+ */
+trait TraitName
+{
+}

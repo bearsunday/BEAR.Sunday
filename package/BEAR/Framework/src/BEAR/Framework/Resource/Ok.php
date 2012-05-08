479d2c91f4e56b591c6292408d0a642b75b57760
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-05-08 21:09:24 +0900

    Add plain "OK" resource.

---
+++ package/BEAR/Framework/src/BEAR/Framework/Resource/Ok.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  BEAR.Framework
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Framework\Resource;
+
+use BEAR\Resource\Object as ResourceObject;
+use BEAR\Resource\AbstractObject;
+
+/**
+ * Ok page
+ *
+ * @package    BEAR.Framework
+ * @subpackage Page
+ */
+class Ok extends AbstractObject implements ResourceObject
+{
+    public $code = 200;
+    public $headers = [];
+    public $body = '';
+
+    public function __construct()
+    {
+    }
+}

6c3b3f58f64b4938ea1487f38390025f017d7d57
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-05-12 04:01:18 +0900

    @Form annotation.

---
+++ apps/sandbox/Annotation/Form.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * BEAR.Framework
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace sandbox\Annotation;
+
+/**
+ * Form
+ *
+ * @Annotation
+ * @Target({"METHOD"})
+ *
+ * @package    sandbox
+ * @subpackage Annotation
+ */
+final class Form
+{
+}

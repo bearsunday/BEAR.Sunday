d92788cf9e424a1f4bfd213442c35fc832de4c51
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-05-12 21:56:21 +0900

    Add /blog/posts/edit.

---
+++ apps/sandbox/Resource/Page/Blog/Posts/Edit.php
@@ -0,0 +1,66 @@
+<?php
+namespace sandbox\Resource\Page\Blog\Posts;
+
+use BEAR\Resource\Client as Resource;
+use BEAR\Resource\Annotation\Provides;
+
+use BEAR\Framework\Resource\AbstractPage as Page;
+use BEAR\Framework\Link\View as View;
+use BEAR\Framework\Inject\WebContextInject;
+use BEAR\Framework\Inject\ResourceInject;
+use BEAR\Framework\Args;
+use BEAR\Framework\Annotation\Cache;
+use BEAR\Framework\Annotation\CacheUpdate;
+use BEAR\Framework\Annotation\Html;
+
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Named;
+use sandbox\Annotation\Form;
+
+/**
+ * @Html
+ */
+class Edit extends Page
+{
+    use ResourceInject;
+
+    public $body = [
+        'errors' => ['title' => '', 'body' => ''],
+        'submit' => ['title' => '', 'body' => '']
+    ];
+
+    /**
+     * Get
+     *
+     */
+    public function onGet($id)
+    {
+        $this['submit'] = $this->resource->get->uri('app://self/posts')->withQuery(['id' => $id])->request()->body;
+        $this['id'] = $id;
+        return $this;
+    }
+
+    /**
+     * Put
+     *
+     * @param int    $id
+     * @param string $title
+     * @param string $body
+     *
+     * @Form
+     */
+    public function onPut($id, $title, $body)
+    {
+        // create post
+        $this->resource
+        ->put
+        ->uri('app://self/posts')
+        ->withQuery(['id' => $id, 'title' => $title, 'body' => $body])
+        ->eager->request();
+
+        // redirect
+        $this->code = 303;
+        $this->headers = ['Location' => '/blog/posts'];
+        return $this;
+    }
+}

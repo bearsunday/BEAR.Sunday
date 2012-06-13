ba8501e7b8603ced1b5a060541740837ab43e744
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-06-13 11:44:41 +0900

    Accept full path.

--- apps/sandbox/htdocs/_bear/edit/input.php
+++ apps/sandbox/htdocs/_bear/edit/input.php
@@ -1,15 +1,21 @@
 <?php
 /**
  * Return validated path info
- * 
+ *
  * @return array [$fullPath, $line, $relativePath]
  */
 
 // input
-$relativePath = isset($_REQUEST['file']) ?  $_REQUEST['file'] : false;
 $line = isset($_GET['line']) ? $_GET['line'] : 0;
+$path = isset($_REQUEST['file']) ?  $_REQUEST['file'] : false;
 $rootDir = isset($_ENV['SUNDAY_ROOT']) ? $_ENV['SUNDAY_ROOT'] : dirname(dirname(dirname(dirname(dirname(__DIR__)))));
-$fullPath = $rootDir . '/' . $relativePath;
+if (file_exists($path)) {
+    $fullPath = $path;
+    $relativePath = str_replace($rootDir, '', $fullPath);
+} else {
+    $fullPath = $rootDir . '/' . $path;
+    $relativePath = $path;
+}
 // validate
 if (!is_readable($fullPath)) {
 	throw new \InvalidArgumentException("Not found. {$fullPath} is not readable.");

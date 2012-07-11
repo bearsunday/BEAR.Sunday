4474cf28a4e5cedddbaa4d96b1f7d5d71ca763b9
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-07-12 04:00:09 +0900

    More $_ENV dependency.

--- apps/sandbox/scripts/config.php
+++ apps/sandbox/scripts/config.php
@@ -2,21 +2,23 @@
 /**
  * Application config
  */
+$id = isset($_ENV['BEAR_DB_ID']) ? $_ENV['BEAR_DB_ID'] : 'root';
+$password = isset($_ENV['BEAR_DB_PASSOWRD']) ? $_ENV['BEAR_DB_PASSOWRD'] : '';
 return [
     'master_db' => [
         'driver' => 'pdo_mysql',
         'host' => 'localhost',
         'dbname' => 'blogbear',
-        'user' => 'root',
-        'password' => null,
+        'user' => $id,
+        'password' => $password,
         'charset' => 'UTF8'
     ],
     'slave_db' => [
         'driver' => 'pdo_mysql',
         'host' => 'localhost',
         'dbname' => 'blogbear',
-        'user' => 'root',
-        'password' => null,
+        'user' => $id,
+        'password' => $password,
         'charset' => 'UTF8'
     ]
 ];
--- apps/sandbox/scripts/test.config.php
+++ apps/sandbox/scripts/test.config.php
@@ -2,21 +2,23 @@
 /**
  * Config for unit test
  */
+$id = isset($_ENV['BEAR_DB_ID']) ? $_ENV['BEAR_DB_ID'] : 'root';
+$password = isset($_ENV['BEAR_DB_PASSOWRD']) ? $_ENV['BEAR_DB_PASSOWRD'] : '';
 return [
     'master_db' => [
         'driver' => 'pdo_mysql',
         'host' => 'localhost',
         'dbname' => 'blogbeartest',
-        'user' => 'root',
-        'password' => null,
+        'user' => $id,
+        'password' => $password,
         'charset' => 'UTF8'
     ],
     'slave_db' => [
         'driver' => 'pdo_mysql',
         'host' => 'localhost',
         'dbname' => 'blogbeartest',
-        'user' => 'root',
-        'password' => null,
+        'user' => $id,
+        'password' => $password,
         'charset' => 'UTF8'
     ]
 ];

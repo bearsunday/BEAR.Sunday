b5bd2cd8c5fc0f62915b9fca8f3eec81bce7f77e
Shigeyuki INOUE aka kaepapa <kaepapa@gmail.com>
2012-07-12 14:18:00 +0900

    fix typo.

--- tests/scripts/db.php
+++ tests/scripts/db.php
@@ -8,5 +8,5 @@
  * @global $_ENV['BEAR_DB_PASSOWRD']
  */
 $id = isset($_ENV['BEAR_DB_ID']) ? $_ENV['BEAR_DB_ID'] : 'root';
-$password = isset($_ENV['BEAR_DB_PASSOWRD']) ? $_ENV['BEAR_DB_PASSOWRD'] : '';
+$password = isset($_ENV['BEAR_DB_PASSWORD']) ? $_ENV['BEAR_DB_PASSWORD'] : '';
 return new \PDO("mysql:host=localhost; dbname=blogbeartest", $id, $password);

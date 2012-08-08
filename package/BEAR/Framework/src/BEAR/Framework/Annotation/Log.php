5091b73bd52d0617f6f911dbd80e12c1ecb410fb
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-08-09 02:57:52 +0900

    Add @Log annotation.

---
+++ package/BEAR/Framework/src/BEAR/Framework/Annotation/Log.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * BEAR.Framework
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Framework\Annotation;
+
+/**
+ * Log
+ *
+ * @Annotation
+ * @Target({"METHOD","CLASS"})
+ *
+ * @package    BEAR.Framework
+ * @subpackage Annotation
+ */
+final class Log
+{
+}

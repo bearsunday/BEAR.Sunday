c051e71f19de489f8f84d228c5f322649e24f5b5
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-08-15 17:57:18 +0900

    Check installation before tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,15 @@
 <?php
-// vendor
+error_reporting(E_ALL);
+
+ob_start(); // to hide chekcer message
+$isInstallOk = require dirname(__DIR__) . '/scripts/check_install.php';
+if (! $isInstallOk) {
+    echo "Please fix the install problem before tests." . PHP_EOL;
+    exit(1);
+}
+ob_end_clean();
+
+// loader
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 require 'PHPUnit/Extensions/Database/TestCase.php';

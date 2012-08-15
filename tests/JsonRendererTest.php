6fb2c50a4f106c4ae9d864593f1be54847b3c6e2
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-08-15 18:26:32 +0900

    Use lower case for Aura package.

--- tests/JsonRendererTest.php
+++ tests/JsonRendererTest.php
@@ -28,7 +28,7 @@ class JsonRendererTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        $signal = require dirname(__DIR__) . '/vendor/Aura/Signal/scripts/instance.php';
+        $signal = require dirname(__DIR__) . '/vendor/aura/signal/scripts/instance.php';
         $request = new Request(new Invoker(new Config(new Annotation(new Definition)), new Linker(new Reader), $signal));
         $request->method = 'get';
         $this->testResource = new Ok;

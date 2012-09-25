4854454f63bd5c85d947a21808ae95a44da6117d
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-09-26 02:10:46 +0900

    More strict check.

--- apps/sandbox/htdocs/_dev/edit/input.php
+++ apps/sandbox/htdocs/_dev/edit/input.php
@@ -9,14 +9,18 @@
 $line = isset($_GET['line']) ? $_GET['line'] : 0;
 $path = isset($_REQUEST['file']) ?  $_REQUEST['file'] : false;
 $rootDir = isset($_ENV['SUNDAY_ROOT']) ? $_ENV['SUNDAY_ROOT'] : dirname(dirname(dirname(dirname(dirname(__DIR__)))));
+
+// disallow full path
 $fullPath = $rootDir . '/' . $path;
 $fullPath = realpath($fullPath);
 $relativePath = $path;
-// validate
+
+// readable ?
 if (!is_readable($fullPath)) {
     throw new \InvalidArgumentException("Not found. {$fullPath} is not readable.");
 }
-if (strpos($fullPath, $rootDir) === false) {
+// allow only under project
+if (strpos($fullPath, $rootDir) !== 0) {
     throw new \OutOfRangeException($fullPath);
 }
 

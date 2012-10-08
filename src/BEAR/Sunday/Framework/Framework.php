eab200f7cc6919e6263fe48fd1ae3f6ec0f582a6
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-09 01:45:48 +0900

    Remove autoloadr from Framework.

--- src/BEAR/Sunday/Framework/Framework.php
+++ src/BEAR/Sunday/Framework/Framework.php
@@ -53,30 +53,5 @@ final class Framework
      */
     public function setLoader($namespace, $appDir, array $namespaces = [])
     {
-        static $loader;
-
-        if (! is_null($loader)) {
-            // unregister for another app
-            spl_autoload_unregister([$loader, 'load']);
-        }
-        $system = self::$systemRoot;
-        $packageDir = dirname(dirname($appDir));
-        include_once $system . '/scripts/core_loader.php';
-        include_once $packageDir . '/vendor/aura/autoload/src.php';
-        $loader = new Loader;
-        $loader->setMode(Loader::MODE_DEBUG);
-        $autloadNamespaces = include $packageDir . '/vendor/composer/autoload_namespaces.php';
-        $autloadNamespaces[$namespace] = dirname($appDir);
-        $autloadNamespaces += $namespaces;
-        $loader->setPaths($autloadNamespaces);
-        $classes = include $packageDir . '/vendor/composer/autoload_classmap.php';
-        $loader->setClasses($classes);
-        $loader->register();
-        AnnotationRegistry::registerAutoloadNamespace('Ray\Di\Di\\', $packageDir . '/vendor/ray/di/src/');
-        AnnotationRegistry::registerAutoloadNamespace('BEAR\Resource\Annotation\\', $packageDir . '/vendor/bear/resource/src/');
-        AnnotationRegistry::registerAutoloadNamespace('BEAR\Sunday\Annotation\\', $packageDir . '/src/');
-        AnnotationRegistry::registerAutoloadNamespace($namespace . '\Annotation', dirname($appDir));
-
-        return $this;
     }
 }

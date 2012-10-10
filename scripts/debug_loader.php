713da2b05c4b539c4b1bd9171f4e2814ddcec827
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-10 20:16:24 +0900

    Fix loader path.

--- scripts/debug_loader.php
+++ scripts/debug_loader.php
@@ -1,14 +1,9 @@
 <?php
-require_once $system . '/vendor/facebook/xhprof/xhprof_lib/utils/xhprof_lib.php';
-require_once $system . '/vendor/facebook/xhprof/xhprof_lib/utils/xhprof_runs.php';
+require_once dirname(__DIR__) . '/vendor/facebook/xhprof/xhprof_lib/utils/xhprof_lib.php';
+require_once dirname(__DIR__) . '/vendor/facebook/xhprof/xhprof_lib/utils/xhprof_runs.php';
 
 // reroute another PHP file
 preg_match('/\/_dev(.+)$/', $_SERVER['SCRIPT_FILENAME'], $matches);
 if (! $matches) {
-    return;
-}
-$scrpitFile = __DIR__  . $matches[0];
-if (PHP_SAPI !== 'cli' && file_exists($scrpitFile)) {
-    include $scrpitFile;
-    exit(0);
+    return false;
 }

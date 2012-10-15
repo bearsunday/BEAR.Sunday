3884080a56a0f6fa87b945af32b1b04f30026938
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-15 19:13:25 +0900

    BEAR.Framework to BEAR.Sunday

--- src/BEAR/Sunday/Module/Log/MonologModule.php
+++ src/BEAR/Sunday/Module/Log/MonologModule.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * This file is part of the BEAR.Framework package
+ * This file is part of the BEAR.Sunday package
  *
- * @package BEAR.Framework
+ * @package BEAR.Sunday
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
 namespace BEAR\Sunday\Module\Log;
@@ -12,7 +12,7 @@ use Ray\Di\AbstractModule;
 /**
  * Monolog module
  *
- * @package    BEAR.Framework
+ * @package    BEAR.Sunday
  * @subpackage Module
  */
 class MonologModule extends AbstractModule

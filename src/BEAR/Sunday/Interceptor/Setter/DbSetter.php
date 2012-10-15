9bc3dcf0d6134afbb1ea94c8702c4327d58a888d
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-15 19:09:07 +0900

    Db runtime setter.

--- src/BEAR/Sunday/Interceptor/Setter/DbSetter.php
+++ src/BEAR/Sunday/Interceptor/Setter/DbSetter.php
@@ -1,8 +1,38 @@
 <?php
-    <?php
 /**
- * This file is part of the {package} package
+ * This file is part of the BEAR.Sunday package
  *
- * @package {package}
+ * @package BEAR.Sunday
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
+namespace BEAR\Sunday\Interceptor\Setter;
+
+use Doctrine\DBAL\Driver\Connection;
+
+/**
+ * Set Db
+ *
+ * @package    BEAR.Sunday
+ * @subpackage Inject
+ */
+trait DbSetter
+{
+    /**
+     * DB
+     *
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $db;
+
+    /**
+     * Set DB
+     *
+     * @param Connection $db
+     *
+     * @return void
+     */
+    public function setDb(Connection $db = null)
+    {
+        $this->db = $db;
+    }
+}

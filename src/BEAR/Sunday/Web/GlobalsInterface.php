b8d2f6bae9e110360e30c0a0040cc385b4e0659b
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-26 01:10:22 +0900

    add framework module interface

---
+++ src/BEAR/Sunday/Web/GlobalsInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the BEAR.Sunday package
+ *
+ * @package BEAR.Sunday
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Sunday\Web;
+
+/**
+ * Interface GLOBAL
+ *
+ * @package    BEAR.Sunday
+ * @subpackage Web
+ */
+interface GlobalsInterface
+{
+}

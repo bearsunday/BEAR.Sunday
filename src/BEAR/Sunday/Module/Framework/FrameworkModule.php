f034ed34f82e5ba048b8fc4c0deaacffa04eb015
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-12-07 04:27:34 +0900

    move exception handler to BEAR.Package

--- src/BEAR/Sunday/Module/Framework/FrameworkModule.php
+++ src/BEAR/Sunday/Module/Framework/FrameworkModule.php
@@ -34,7 +34,6 @@ class FrameworkModule extends AbstractModule
         $this->install(new Module\Code\CachedAnnotationModule);
         $this->install(new Module\Signal\SignalModule);
         $this->install(new Module\Resource\ResourceModule($this->dependencyInjector));
-        $this->install(new Module\ExceptionHandle\HandleModule);
         $this->install(new Module\Output\ConsoleModule);
         $this->install(new Module\Http\GuzzleModule);
         $this->install(new Module\TemplateEngine\ProdRendererModule);

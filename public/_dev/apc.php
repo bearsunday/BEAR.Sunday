3a90b21df0af8def459d6f4f360531140398f025
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-12-10 18:30:13 +0900

    rm public folder (move to BEAR.Package)

--- public/_dev/apc.php
+++
@@ -1,4 +0,0 @@
-<?php
-require dirname(dirname(__DIR__)) . '/scripts/instance.php';
-require __DIR__ . '/libs/debuglib.php';
-require __DIR__ . '/apc/apc.php';
--- public/_dev/graph/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require dirname(dirname(dirname(__DIR__))) . '/scripts/instance.php';
-require dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/vendor/printo/printo/src.php';
-
-$app = apc_fetch($_GET['app']);
-print_o($app);

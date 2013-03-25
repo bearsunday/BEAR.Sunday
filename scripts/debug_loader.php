fbc07d69ad034958bd2ff4c38d164d89051469e3
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-03-25 23:16:08 +0900

    remove unused files

--- scripts/debug_loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__DIR__) . '/vendor/facebook/xhprof/xhprof_lib/utils/xhprof_lib.php';
-require_once dirname(__DIR__) . '/vendor/facebook/xhprof/xhprof_lib/utils/xhprof_runs.php';
-
-// reroute another PHP file
-preg_match('/\/_dev(.+)$/', $_SERVER['SCRIPT_FILENAME'], $matches);
-if (!$matches) {
-    return false;
-}

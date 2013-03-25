70e71a28b74122086d072ded62568205e5316e94
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-03-25 23:06:27 +0900

    use composer autoloader for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,19 +4,8 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 error_reporting(E_ALL);
 
-ob_start(); // to hide checker message
-$isInstallOk = require dirname(__DIR__) . '/scripts/check_env.php';
-if (!$isInstallOk) {
-    echo "Please fix the install problem before tests." . PHP_EOL;
-    exit(1);
-}
-ob_end_clean();
-
 // loader
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-require 'PHPUnit/Extensions/Database/TestCase.php';
-$system = dirname(__DIR__);
-AnnotationRegistry::registerAutoloadNamespace('Ray\Di\Di\\', $system . '/vendor/ray/di/src');
-AnnotationRegistry::registerAutoloadNamespace('BEAR\Resource\Annotation\\', $system . '/vendor/bear/resource/src/');
-AnnotationRegistry::registerAutoloadNamespace('BEAR\Sunday\Annotation\\', $system . '/src/');
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+/** @var $loader \Composer\Autoload\ClassLoader */
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+$loader->add('BEAR\Sunday' , __DIR__);

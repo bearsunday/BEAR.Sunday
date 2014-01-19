f31cfa7725aa8932175c45db6b98eef260ba429e
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-19 13:15:41 +0900

    add PsrLoggerInject
    suport psr/log interface

---
+++ tests/BEAR/Sunday/Inject/PsrLoggerInjectTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BEAR\Sunday\Inject;
+
+use Psr\Log\LoggerInterface;
+use Ray\Di\Injector;
+use BEAR\Sunday\Inject\PsrLoggerInject;
+use Ray\Di\AbstractModule;
+
+class PsrLoggerApplication
+{
+    use PsrLoggerInject;
+
+    public function returnDependency()
+    {
+        return $this->logger;
+    }
+
+}
+
+class DummyLogger implements LoggerInterface
+{
+    public function emergency($message, array $context = array())
+    {
+    }
+    public function alert($message, array $context = array())
+    {
+    }
+    public function critical($message, array $context = array())
+    {
+    }
+    public function error($message, array $context = array())
+    {
+    }
+    public function warning($message, array $context = array())
+    {
+    }
+    public function notice($message, array $context = array())
+    {
+    }
+    public function info($message, array $context = array())
+    {
+    }
+    public function debug($message, array $context = array())
+    {
+    }
+    public function log($level, $message, array $context = array())
+    {
+    }
+}
+
+class PsrLoggerModule extends AbstractModule
+{
+    protected function configure()
+    {
+        $this->bind('Psr\Log\LoggerInterface')->to(__NAMESPACE__ . '\DummyLogger');
+    }
+}
+
+class PsrLoggerInjectTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInjectTrait()
+    {
+        $app = Injector::create([new PsrLoggerModule])->getInstance(__NAMESPACE__ . '\PsrLoggerApplication');
+        $this->assertInstanceOf( '\BEAR\Sunday\Inject\DummyLogger', $app->returnDependency());
+    }
+}

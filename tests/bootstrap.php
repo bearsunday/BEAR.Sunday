bcebd210a573740d569ca10934fe731b70284f2d
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-13 17:54:33 +0900

    remove manual registerationof loaders

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,17 +4,5 @@
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-error_reporting(E_ALL);
-ini_set('xdebug.max_nesting_level', 300);
-
-// loader
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-/* @var $loader \Composer\Autoload\ClassLoader */
-$loader->addPsr4('BEAR\Sunday\\', [__DIR__, __DIR__ . '/Fake']);
-$loader->addPsr4('FakeVendor\HelloWorld\\', __DIR__ . '/Fake/Apps/FakeVendor/HelloWorld');
-$loader->addPsr4('FakeVendor\HelloWorldX\\', __DIR__ . '/Fake/Apps/FakeVendor/HelloWorldX');
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-
+require dirname(__DIR__) . '/vendor/autoload.php';
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';

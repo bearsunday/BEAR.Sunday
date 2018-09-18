d96a20ff555fa19a4c4ef0d47a1a77e3129a4315
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-09-19 00:17:55 +0900

    add NullMatch

---
+++ src/Extension/Router/NullMatch.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the BEAR.Sunday package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Sunday\Extension\Router;
+
+class NullMatch extends RouterMatch
+{
+}

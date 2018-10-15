91bf39ffee4ab874681371d8340f7f3b2b01ff22
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-10-15 11:22:15 +0900

    update demo code

--- demo/bin/index.php
+++ demo/bin/index.php
@@ -6,6 +6,8 @@ declare(strict_types=1);
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
+use BEAR\Resource\ResourceObject;
+use BEAR\Sunday\Extension\Application\AbstractApp;
 use BEAR\Sunday\Extension\Application\AppInterface;
 use MyVendor\HelloWorld\AppModule;
 use Ray\Di\Injector;
@@ -13,12 +15,12 @@ use Ray\Di\Injector;
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = (new Injector(new AppModule))->getInstance(AppInterface::class);
+/* @var AbstractApp $app */
 try {
-    $page = $app->resource
-        ->get
-        ->uri('page://self/index')(['name' => 'BEAR.Sunday'])
-        ->transfer($app->responder, $_SERVER);
+    $response = $app->resource->get('page://self/index', ['name' => 'BEAR.Sunday']);
+    /* @var ResourceObject $response */
+    $response->transfer($app->responder, $_SERVER);
 } catch (\Exception $e) {
-    error_log($e);
+    error_log((string) $e);
     exit(1);
 }

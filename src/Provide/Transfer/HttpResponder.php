747eaf340efdf8a1b5f726412992af4b984c0123
Toshiyuki FUJITA <kalibora@gmail.com>
2019-01-03 19:53:11 +0900

    Support If-None-Match header

--- src/Provide/Transfer/HttpResponder.php
+++ src/Provide/Transfer/HttpResponder.php
@@ -14,6 +14,10 @@ class HttpResponder implements TransferInterface
      */
     public function __invoke(ResourceObject $ro, array $server)
     {
+        if ($this->respondeIfETagMatch($ro, $server)) {
+            return;
+        }
+
         unset($server);
         // render
         if (! $ro->view) {
@@ -30,5 +34,39 @@ class HttpResponder implements TransferInterface
 
         // body
         echo $ro->view;
+    }
+
+    private function respondeIfETagMatch(ResourceObject $ro, array $server) : bool
+    {
+        if (! isset($server['HTTP_IF_NONE_MATCH'])
+            || ! isset($ro->headers['ETag'])
+            || $server['HTTP_IF_NONE_MATCH'] !== $ro->headers['ETag']
+        ) {
+            return false;
+        }
+
+        // See: https://httpwg.org/specs/rfc7232.html#status.304
+        $mustGenerateHeaders = [
+            'Cache-Control',
+            'Content-Location',
+            'Date',
+            'ETag',
+            'Expires',
+            'Vary',
+        ];
+
+        // header
+        foreach ($ro->headers as $label => $value) {
+            if (! in_array($label, $mustGenerateHeaders, true)) {
+                continue;
+            }
+
+            header("{$label}: {$value}", false);
+        }
+
+        // code
+        http_response_code(304);
+
+        return true;
     }
 }
--- tests/Fake/Provide/Transfer/FakeHttpResponder.php
+++ tests/Fake/Provide/Transfer/FakeHttpResponder.php
@@ -24,13 +24,55 @@ class FakeHttpResponder implements TransferInterface
 
     public function __invoke(ResourceObject $ro, array $server)
     {
+        if ($this->respondeIfETagMatch($ro, $server)) {
+            return;
+        }
+
+        // render
         if (! $ro->view) {
             self::$body = $ro->toString();
         }
+
         // header
         foreach ($ro->headers as $label => $value) {
             header("{$label}: {$value}", false);
         }
-        self::$code = $ro->code;
+
+        // code
+        http_response_code($ro->code);
+    }
+
+    private function respondeIfETagMatch(ResourceObject $ro, array $server) : bool
+    {
+        if (! isset($server['HTTP_IF_NONE_MATCH'])
+            || ! isset($ro->headers['ETag'])
+            || $server['HTTP_IF_NONE_MATCH'] !== $ro->headers['ETag']
+        ) {
+            return false;
+        }
+
+        // See: https://httpwg.org/specs/rfc7232.html#status.304
+        $mustGenerateHeaders = [
+            'Cache-Control',
+            'Content-Location',
+            'Date',
+            'ETag',
+            'Expires',
+            'Vary',
+        ];
+
+        // header
+        foreach ($ro->headers as $label => $value) {
+            if (! in_array($label, $mustGenerateHeaders, true)) {
+                continue;
+            }
+
+            header("{$label}: {$value}", false);
+        }
+
+        // code
+        http_response_code(304);
+
+        return true;
     }
 }

194ce428413a12463238dbd8973417e34c6e08c9
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-06-22 20:23:21 +0900

    set 'Content-Type'

--- src/Provide/Error/VndError.php
+++ src/Provide/Error/VndError.php
@@ -64,7 +64,7 @@ final class VndError implements ErrorInterface
      */
     public function transfer()
     {
-        $this->errorPage->headers['content-type'] = self::CONTENT_TYPE;
+        $this->errorPage->headers['Content-Type'] = self::CONTENT_TYPE;
         $this->transfer->__invoke($this->errorPage, []);
     }
 

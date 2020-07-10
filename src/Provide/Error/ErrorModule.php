35b3be4bdb2a362f331daab1ae8defd5e21e4697
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-07-11 06:43:11 +0900

    Bind ThrowableHandlerIntreface in ErrorModule

--- src/Provide/Error/ErrorModule.php
+++ src/Provide/Error/ErrorModule.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace BEAR\Sunday\Provide\Error;
 
 use BEAR\Sunday\Extension\Error\ErrorInterface;
+use BEAR\Sunday\Extension\Error\ThrowableHandlerInterface;
+use BEAR\Sunday\Extension\Provide\ThrowableHandler;
 use Ray\Di\AbstractModule;
 
 class ErrorModule extends AbstractModule
@@ -15,5 +17,6 @@ class ErrorModule extends AbstractModule
     protected function configure() : void
     {
         $this->bind(ErrorInterface::class)->to(VndError::class);
+        $this->bind(ThrowableHandlerInterface::class)->to(ThrowableHandler::class);
     }
 }

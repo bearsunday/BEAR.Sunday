817d1a8b3136406d9310785741f4e258badc1ade
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-07-11 06:42:40 +0900

    Add ThrowableHandler

---
+++ src/Extension/Error/ThrowableHandlerInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Sunday\Extension\Error;
+
+use BEAR\Sunday\Extension\Router\RouterMatch as Request;
+use Throwable;
+
+interface ThrowableHandlerInterface
+{
+    /**
+     * Handle Throwable
+     */
+    public function handle(Throwable $e, Request $request) : self;
+
+    /**
+     * Transfer error page
+     */
+    public function transfer() : void;
+}
---
+++ src/Provide/Error/ThrowableHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Sunday\Provide\Error;
+
+use BEAR\Sunday\Extension\Error\ErrorInterface;
+use BEAR\Sunday\Extension\Error\ThrowableHandlerInterface;
+use BEAR\Sunday\Extension\Router\RouterMatch as Request;
+use const E_ERROR;
+use Error;
+use ErrorException;
+use Exception;
+use Throwable;
+
+final class ThrowableHandler implements ThrowableHandlerInterface
+{
+    /**
+     * @var ErrorInterface
+     */
+    private $error;
+
+    public function __construct(ErrorInterface $error)
+    {
+        $this->error = $error;
+    }
+
+    public function handle(Throwable $e, Request $request) : ThrowableHandlerInterface
+    {
+        $e = $e instanceof Error ? new ErrorException($e->getMessage(), (int) $e->getCode(), E_ERROR, $e->getFile(), $e->getLine()) : $e;
+        assert($e instanceof Exception);
+        $this->error->handle($e, $request);
+
+        return $this;
+    }
+
+    public function transfer() : void
+    {
+        $this->error->transfer();
+    }
+}

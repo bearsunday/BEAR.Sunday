3db62f0de9ef094013b39f05dafbeab204d97204
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-11 04:27:27 +0900

    Add deprecated src

---
+++ src-deprecated/Module/DoctrineAnnotationModule.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Sunday\Module\Annotation;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\CachedReader;
+use Doctrine\Common\Annotations\Reader;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\Cache;
+use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
+
+/**
+ * @deprecated
+ */
+class DoctrineAnnotationModule extends AbstractModule
+{
+    protected function configure(): void
+    {
+        $this->bind(Cache::class)->annotatedWith('annotation_cache')->to(ArrayCache::class)->in(Scope::SINGLETON);
+        $this->bind(Reader::class)->annotatedWith('annotation_reader')->to(AnnotationReader::class)->in(Scope::SINGLETON);
+        $this->bind(Reader::class)->toConstructor(CachedReader::class, 'reader=annotation_reader,cache=annotation_cache')->in(Scope::SINGLETON);
+    }
+}

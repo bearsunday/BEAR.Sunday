e2d7bf9d7010c4519f069b4a5a6d67113abe84a1
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-11-21 11:59:33 +0900

    Soothe rector

--- rector.php
+++ rector.php
@@ -9,10 +9,13 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/demo',
         __DIR__ . '/src',
         __DIR__ . '/src-deprecated',
         __DIR__ . '/tests',
+    ]);
+    $rectorConfig->skip([
+        __DIR__ . '/src/*Interface.php',
+        __DIR__ . '/tests/Provide/Error/ThrowableHandlerTest.php'
     ]);
 
     // register a single rule
